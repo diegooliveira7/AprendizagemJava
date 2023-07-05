ac55b37c61b0dec57c7271c82d25a07de81409cc
diego <diegoolliveira1999@gmail.com>
2023-07-05 10:58:23 -0300

    Criação da classe ProductModel

--- SpringBoot/springboot/src/main/java/com/example/springboot/models/ProductModel.java
+++ SpringBoot/springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,4 +1,43 @@
 package com.example.springboot.models;
 
-public class ProductModel {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel  implements Serializable {//Essa interface é de marcação e não precisa implementar nd
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)//auto geração de identificadores
+    private UUID idProduct; //UUID é um identificador distribuido
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }
