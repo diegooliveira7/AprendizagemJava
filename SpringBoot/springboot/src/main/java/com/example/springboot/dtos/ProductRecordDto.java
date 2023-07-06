9cac911fb6e767c568a4244b235c8289e488f90c
diego <diegoolliveira1999@gmail.com>
2023-07-06 20:40:10 -0300

    post

--- SpringBoot/springboot/src/main/java/com/example/springboot/dtos/ProductRecordDto.java
+++ SpringBoot/springboot/src/main/java/com/example/springboot/dtos/ProductRecordDto.java
@@ -1,8 +1,9 @@
 package com.example.springboot.dtos;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
-public record ProductRecordDto(@NotBlank String name, @NotBlank BigDecimal value) {
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
     //@NotBlank siginifica que o nome n pode ser nulo e nem vázio
 }
